6f09eb72470112362cbc5a2417ed3cc37642c9e6
Tim Damen <tim.damen80@gmail.com>
2023-05-23 16:20:12 +0200

    feat: add ga id

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -26,7 +26,7 @@ const siteMetadata = {
     plausibleDataDomain: '', // e.g. tailwind-nextjs-starter-blog.vercel.app
     simpleAnalytics: false, // true or false
     umamiWebsiteId: '', // e.g. 123e4567-e89b-12d3-a456-426614174000
-    googleAnalyticsId: '', // e.g. UA-000000-2 or G-XXXXXXX
+    googleAnalyticsId: 'G-GMGW9X44KW', // e.g. UA-000000-2 or G-XXXXXXX
     posthogAnalyticsId: '', // posthog.init e.g. phc_5yXvArzvRdqtZIsHkEm3Fkkhm3d0bEYUXCaFISzqPSQ
   },
   newsletter: {
