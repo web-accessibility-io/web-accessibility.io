23e6eb7456953c30096eb7bd25784b6790d721db
Tim Damen <tim.damen80@gmail.com>
2023-05-26 14:39:28 +0200

    chore: upgrade next js

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,7 +6,14 @@ module.exports = {
     node: true,
     es6: true,
   },
-  extends: ['eslint:recommended', 'plugin:prettier/recommended', 'next', 'next/core-web-vitals'],
+  extends: [
+    'eslint:recommended',
+    'prettier',
+    'plugin:prettier/recommended',
+    'plugin:tailwindcss/recommended',
+    'next',
+    'next/core-web-vitals',
+  ],
   rules: {
     'prettier/prettier': 'error',
     'react/react-in-jsx-scope': 'off',
