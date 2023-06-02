9dac80edd0f967e34a11cd9ef6f1363de33c97f0
Tim Damen <tim.damen80@gmail.com>
2023-06-02 11:01:33 +0200

    feat: update nav

--- data/headerNavLinks.js
+++ data/headerNavLinks.js
@@ -1,7 +1,6 @@
 const headerNavLinks = [
   { href: '/playbook', title: 'Playbook' },
   { href: '/blog', title: 'Blog' },
-  { href: '/about', title: 'About' },
 ];
 
 export default headerNavLinks;
