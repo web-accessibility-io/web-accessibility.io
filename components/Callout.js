fc5f3d85a4d54d78d309c4734fcef768addbf2f5
Tim Damen <tim.damen80@gmail.com>
2023-06-05 15:24:49 +0200

    fix: callout dark

--- components/Callout.js
+++ components/Callout.js
@@ -2,8 +2,10 @@ const Callout = ({ children, icon, type = 'default', ...props }) => {
   return (
     <div
       className={`my-6 flex items-center rounded-md border border-l-4 px-3 ${
-        type === 'danger' ? 'border-red-900 bg-red-50' : ''
-      } ${type === 'warning' ? 'border-yellow-900 bg-yellow-50' : ''}`}
+        type === 'warning'
+          ? 'border-yellow-900 bg-yellow-50 dark:bg-yellow-300 dark:text-black'
+          : ''
+      }`}
       {...props}
     >
       {icon && <span className="mr-4 text-2xl">{icon}</span>}
