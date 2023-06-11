07e9cb6db492d5d7d2c9376220a81867032096da
Tim Damen <tim.damen80@gmail.com>
2023-06-11 13:34:23 +0200

    feat: performance optimisation

--- next.config.js
+++ next.config.js
@@ -56,6 +56,10 @@ const securityHeaders = [
 module.exports = nextTranslate(
   withBundleAnalyzer({
     reactStrictMode: true,
+    experimental: {
+      browsersListForSwc: true,
+      legacyBrowsers: false,
+    },
     pageExtensions: ['js', 'jsx', 'md', 'mdx'],
     eslint: {
       dirs: ['pages', 'components', 'lib', 'layouts', 'scripts'],
